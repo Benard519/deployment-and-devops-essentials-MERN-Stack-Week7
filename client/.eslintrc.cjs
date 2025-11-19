067f2966d173db16aff1780dbfd6f0bbe386e757
Benard Atika <benardmachuka08@gmail.com>
2025-11-19 18:15:41 +0300

    Add files via upload

---
+++ client/.eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2022: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react-hooks/recommended'],
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'react/prop-types': 'off',
+  },
+};
+
+
